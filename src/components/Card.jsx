11414ebcc9a7277c9a56129a667136f20e65961b
t0nylombardi <alombardi.331@gmail.com>
2024-05-23 15:11:55 -0400

    created imdb clone

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card({name, movies}) {
+  return (
+    <article>
+      <h2>{name}</h2>
+      <ul>
+        {movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </article>
+  )
+}
+
+export default Card
